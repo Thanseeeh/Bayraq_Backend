524ee50bb17d2f2358a1f0ebe91537295d7615eb
Thanseeh yaseen <yaseenkdr27@gmail.com>
2024-08-19 10:44:40 +0530

    Navbar Pages ADDED

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,10 @@
 # Client Urls
 from django.urls import path
-from .views import Home
+from .views import Home, Categories, AboutUs, Contact
 
 urlpatterns = [
-    path('', Home.as_view(), name="home"),
+    path('', Home.as_view(), name='home'),
+    path('categories/', Categories.as_view(), name='categories'),
+    path('aboutus/', AboutUs.as_view(), name='aboutus'),
+    path('contact', Contact.as_view(), name='contact'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -2,8 +2,30 @@
 from rest_framework.views import APIView
 from django.shortcuts import render
 
+
 # HomePage
 class Home(APIView):
     def get(self, request):
         context = {}
         return render(request, 'index.html', context)
+
+
+# CategoriesPage
+class Categories(APIView):
+    def get(self, request):
+        context = {}
+        return render(request, 'categories.html', context)
+    
+
+# AboutUsPage
+class AboutUs(APIView):
+    def get(self, reqeust):
+        context = {}
+        return render(reqeust, 'about_us.html', context)
+    
+
+# ContactPage
+class Contact(APIView):
+    def get(self, reqeust):
+        context = {}
+        return render(reqeust, 'contact.html', context)
